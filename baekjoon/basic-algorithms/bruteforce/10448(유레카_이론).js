ccfc7c157545588e36dbc64deec5177dbc6b2470
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-08-15 22:12:49 +0900

    10448 completed

--- baekjoon/basic-algorithms/bruteforce/10448(유레카_이론).js
+++ baekjoon/basic-algorithms/bruteforce/10448(유레카_이론).js
@@ -1,5 +1,6 @@
 // 삼각수 Tn = 1 + 2 + 3 + ... + n = n(n+1)/2
 // 가우스의 정리: 모든 자연수는 세 개 이하의 삼각수의 합으로 나타낼 수 있음
+// 1000보다 작거나 같은 삼각수를 모두 계산 후 저장
 // 자연수 중에서 정확히 세 개의 삼각수의 합으로 표현될 수 있는 수를 판별(각 삼각수는 다르지 않아도 됨)
 // 각 테스트케이스에 대해서 조건에 맞으면 1, 아니면 0을 반환
 
@@ -14,6 +15,7 @@ const T = input[0];
 const answer = Array(T + 1).fill(0);
 const TRI_NUM = [];
 
+// 1000보다 작거나 같은 삼각수 모두 계산 후 저장
 let n = 0;
 while (1) {
   const tri = (n * (n + 1)) / 2;
@@ -22,29 +24,35 @@ while (1) {
   TRI_NUM.push(tri);
   n++;
 }
+const RANGE = TRI_NUM.length;
 
-const TRI_LENGTH = TRI_NUM.length - 1;
-
-for (let i = 1; i <= T; i++) {
-  const K = input[i];
-
-  for (let j = 1; TRI_NUM[j] < K; j++) {
-    const K_1 = K - TRI_NUM[j];
-    console.log(i, K_1);
-    if (K_1 > 0) {
-      for (let k = 1; TRI_NUM[k] < K_1; k++) {
-        const K_2 = K_1 - TRI_NUM[k];
-
-        if (K_2 > 0) {
-          for (let l = 1; TRI_NUM[l] <= K_2; l++) {
-            const K_3 = K_2 - TRI_NUM[l];
-
-            if (K_3 === 0) answer[i] === 1;
-          }
-        }
+// Functions
+function isTriSum(caseNum, level, caseOrder) {
+  for (let i = 1; i <= RANGE; i++) {
+    const newCaseNum = caseNum - TRI_NUM[i];
+    if (level === 3) {
+      if (newCaseNum === 0) {
+        answer[caseOrder] = 1;
+        return;
       }
+      continue;
     }
+    if (newCaseNum <= 0) return;
+
+    isTriSum(newCaseNum, level + 1, caseOrder);
   }
 }
 
-console.log(answer);
+function solve() {
+  // 각각의 케이스에 대해 삼각수를 빼는 계산
+  for (let i = 1; i <= T; i++) {
+    const K = input[i];
+    isTriSum(K, 1, i);
+  }
+
+  for (let i = 1; i <= T; i++) {
+    console.log(answer[i]);
+  }
+}
+
+solve();
