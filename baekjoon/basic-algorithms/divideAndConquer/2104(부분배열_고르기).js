380ca6fe437b1a6738fb8cdd2273ecbd339302b2
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-08-31 22:14:09 +0900

    1463 solved
    bottom up

---
+++ baekjoon/basic-algorithms/divideAndConquer/2104(부분배열_고르기).js
@@ -0,0 +1,19 @@
+// const INPUT_1 = ["6", "3 1 6 4 5 2"];
+
+// const input = INPUT_1;
+
+// // Solution
+// function solution(input) {
+//   // Variables
+//   const N = parseInt(input[0]);
+
+//   // Functions
+//   function solve() {
+//     //
+//   }
+
+//   // Run
+//   solve();
+// }
+
+// solution(input);
