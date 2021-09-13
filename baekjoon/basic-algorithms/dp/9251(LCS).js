0ff1c629902ce785343b012fe825f679b2bfacf3
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-09-13 20:45:53 +0900

    9251 created

---
+++ baekjoon/basic-algorithms/dp/9251(LCS).js
@@ -0,0 +1,18 @@
+const INPUT_1 = [];
+
+const input = INPUT_1;
+
+// Solution
+function solution(input) {
+  // Variables
+
+  // Functions
+  function solve() {
+    // initialize variables
+  }
+
+  // Run
+  solve();
+}
+
+solution(input);
