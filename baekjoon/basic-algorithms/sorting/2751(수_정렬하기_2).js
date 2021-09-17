56fe5ce307a3fd396aa47bdf48b43ba136da2e6c
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-09-17 12:18:18 +0900

    2751 solved

---
+++ baekjoon/basic-algorithms/sorting/2751(수_정렬하기_2).js
@@ -0,0 +1,23 @@
+// const INPUT_1 = [];
+
+// const input = INPUT_1;
+
+// // Solution
+// function solution(input) {
+//   // Variables
+
+//   function solution(input) {
+//     const N = parseInt(input[0]);
+//     const numbers = input.slice(1).map(e => parseInt(e));
+//     let answer = '';
+
+//     numbers.sort((a, b) => a - b);
+//     for(let i = 0; i < N; i++) {
+//         answer += numbers[i];
+//         if(i < N - 1) answer += '\n';
+//     }
+
+//     console.log(answer);
+// }
+
+// solution(input);
