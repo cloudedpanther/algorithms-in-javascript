a5ef0458f983b4312b4db0b9a96d8d6f5f8b1f1f
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-09-19 20:33:01 +0900

    I prefer this one
    back to the previouse one again

--- baekjoon/basic-algorithms/자료구조/9093(단어_뒤집기).js
+++ baekjoon/basic-algorithms/자료구조/9093(단어_뒤집기).js
@@ -1,40 +1,42 @@
-const INPUT_1 = ["2", "I am happy today ", "We want to win the first prize"];
-// I ma yppah yadot
-// eW tnaw ot niw eht tsrif ezirp
+// const INPUT_1 = ["2", "I am happy today ", "We want to win the first prize"];
+// // I ma yppah yadot
+// // eW tnaw ot niw eht tsrif ezirp
 
-const input = INPUT_1;
+// const input = INPUT_1;
 
-// Solution
-function solution(input) {
-  // Variables
-  const T = parseInt(input[0]);
-  const stack = [];
-  let answer = "";
+// // Solution
+// function solution(input) {
+//   // Variables
+//   const T = parseInt(input[0]);
+//   const stack = [];
+//   let answer = "";
 
-  // Functions
-  function emptyStack() {
-    while (stack.length !== 0) {
-      answer += stack.pop();
-    }
-  }
+//   // Functions
+//   function emptyStack() {
+//     while (stack.length !== 0) {
+//       answer += stack.pop();
+//     }
+//   }
 
-  function solve() {
-    for (let i = 1; i < T + 1; i++) {
-      const sentence = input[i].split("");
-      sentence.push("\n");
-      for (let j = 0; j < sentence.length; j++) {
-        const letter = sentence[j];
-        if (letter === " " || letter === "\n") {
-          emptyStack();
-          if (i < T || j < sentence.length - 1) answer += letter;
-        } else stack.push(letter);
-      }
-    }
-    console.log(answer);
-  }
+//   function solve() {
+//     for (let i = 1; i < T + 1; i++) {
+//       const sentence = input[i].split("");
+//       for (let j = 0; j < sentence.length; j++) {
+//         const letter = sentence[j];
+//         if (letter === " ") {
+//           emptyStack();
+//           answer += letter;
+//         } else stack.push(letter);
+//       }
+//       emptyStack();
 
-  // Run
-  solve();
-}
+//       if (i < T) answer += "\n";
+//     }
+//     console.log(answer);
+//   }
 
-solution(input);
+//   // Run
+//   solve();
+// }
+
+// solution(input);
