8bdaea53d8a2e10b266edec51231a41acfd28565
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-09-23 20:56:17 +0900

    9012 with quasi-stack

--- baekjoon/basic-algorithms/자료구조/9012(괄호).js
+++ baekjoon/basic-algorithms/자료구조/9012(괄호).js
@@ -23,18 +23,20 @@ const input = INPUT_1;
 // Solution
 function solution(input) {
   // Variables
-  let stack = [];
   const N = parseInt(input[0]);
 
   // Functions
   function isVPS(index) {
     const seq = input[index].split("");
+    let cnt = 0;
+
     for (let i = 0; i < seq.length; i++) {
-      if (seq[i] === "(") stack.push(1);
-      else if (stack.length === 0) return "NO";
-      else stack.pop();
+      if (seq[i] === "(") cnt++;
+      else cnt--;
+
+      if (cnt < 0) return "NO";
     }
-    if (stack.length !== 0) return "NO";
+    if (cnt !== 0) return "NO";
     else return "YES";
   }
 
@@ -46,7 +48,6 @@ function solution(input) {
     for (let i = 1; i < N + 1; i++) {
       answer += isVPS(i);
       if (i < N) answer += "\n";
-      stack = [];
     }
     console.log(answer);
   }
