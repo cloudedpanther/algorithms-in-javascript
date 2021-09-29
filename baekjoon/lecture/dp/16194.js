c7852f82ddac8bfd4b0d9df4731e8139e63e0450
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-09-29 16:47:05 +0900

    16194 solved
    initial value -1(slightly slower but safe)

--- baekjoon/lecture/dp/16194.js
+++ baekjoon/lecture/dp/16194.js
@@ -1,33 +1,36 @@
-const INPUT_1 = ["4", "1 5 6 7"]; // 4
-const INPUT_2 = ["5", "10 9 8 7 6"]; // 6
-const INPUT_3 = ["10", "1 1 2 3 5 8 13 21 34 55"]; // 5
-const INPUT_4 = ["10", "5 10 11 12 13 30 35 40 45 47"]; // 26
-const INPUT_5 = ["4", "5 2 8 10"]; // 4
-const INPUT_6 = ["4", "3 5 15 16"]; // 10
+// const INPUT_1 = ["4", "1 5 6 7"]; // 4
+// const INPUT_2 = ["5", "10 9 8 7 6"]; // 6
+// const INPUT_3 = ["10", "1 1 2 3 5 8 13 21 34 55"]; // 5
+// const INPUT_4 = ["10", "5 10 11 12 13 30 35 40 45 47"]; // 26
+// const INPUT_5 = ["4", "5 2 8 10"]; // 4
+// const INPUT_6 = ["4", "3 5 15 16"]; // 10
 
-const input = INPUT_6;
+// const input = INPUT_6;
 
-// Solution
-function solution(input) {
-  // Variables
-  const N = parseInt(input[0]);
-  const packs = [0].concat(input[1].split(" ").map((pack) => parseInt(pack)));
-  let dp = new Array(N + 1).fill(1000 * 10000);
+// // Solution
+// function solution(input) {
+//   // Variables
+//   const N = parseInt(input[0]);
+//   const packs = [0].concat(input[1].split(" ").map((pack) => parseInt(pack)));
+//   let dp = new Array(N + 1).fill(-1);
 
-  // Functions
-  function solve() {
-    dp[0] = 0;
+//   // Functions
+//   function solve() {
+//     dp[0] = 0;
 
-    for (let i = 0; i < N; i++) {
-      for (let j = 1; j < N + 1; j++) {
-        if (i + j < N + 1) dp[i + j] = Math.min(dp[i + j], dp[i] + packs[j]);
-      }
-    }
-    console.log(dp[N]);
-  }
+//     for (let i = 0; i < N; i++) {
+//       for (let j = 1; j < N + 1; j++) {
+//         if (i + j < N + 1) {
+//           if (dp[i + j] === -1) dp[i + j] = dp[i] + packs[j];
+//           else dp[i + j] = Math.min(dp[i + j], dp[i] + packs[j]);
+//         }
+//       }
+//     }
+//     console.log(dp[N]);
+//   }
 
-  // Run
-  solve();
-}
+//   // Run
+//   solve();
+// }
 
-solution(input);
+// solution(input);
