2027291c4d3cace9fc1ca0277097080b8a2f881e
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-10-02 22:34:30 +0900

    2193 solved
    with 1dimensional array

--- baekjoon/lecture/dp/2193.js
+++ baekjoon/lecture/dp/2193.js
@@ -1,31 +1,27 @@
-const INPUT_1 = ["90"]; // 2
+// const INPUT_1 = ["3"]; // 2
 
-const input = INPUT_1;
+// const input = INPUT_1;
 
-// Solution
-function solution(input) {
-  // Variables
-  const N = parseInt(input[0]);
-  let dp = new Array(N + 1);
+// // Solution
+// function solution(input) {
+//   // Variables
+//   const N = parseInt(input[0]);
+//   let dp = new Array(N + 1);
 
-  // Functions
-  function solve() {
-    for (let i = 1; i < N + 1; i++) {
-      dp[i] = new Array(2);
-    }
-    dp[1][0] = 0n;
-    dp[1][1] = 1n;
+//   // Functions
+//   function solve() {
+//     dp[0] = 0n;
+//     dp[1] = 1n;
 
-    for (let i = 2; i < N + 1; i++) {
-      dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
-      dp[i][1] = dp[i - 1][0];
-    }
+//     for (let i = 2; i < N + 1; i++) {
+//       dp[i] = dp[i - 1] + dp[i - 2];
+//     }
 
-    console.log(String(dp[N][0] + dp[N][1]));
-  }
+//     console.log(String(dp[N]));
+//   }
 
-  // Run
-  solve();
-}
+//   // Run
+//   solve();
+// }
 
-solution(input);
+// solution(input);
