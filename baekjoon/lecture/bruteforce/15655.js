305ca163d3f29d9894a423ad96c995838a2d1889
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-11-04 18:15:15 +0900

    Create 15655.js

---
+++ baekjoon/lecture/bruteforce/15655.js
@@ -0,0 +1,18 @@
+const INPUT_1 = ["4 2", "9 8 7 1"];
+
+const input = INPUT_1;
+
+// Solution
+const solution = (input) => {
+  // Variables
+
+  // Functions
+  const solve = () => {
+    //
+  };
+
+  // Run
+  solve();
+};
+
+solution(input);
