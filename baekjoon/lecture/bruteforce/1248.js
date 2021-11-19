ba38bb499fa1dacb229a9e6d5c5b9ee3f555042c
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-11-19 23:46:50 +0900

    Update 1248.js

--- baekjoon/lecture/bruteforce/1248.js
+++ baekjoon/lecture/bruteforce/1248.js
@@ -1,43 +1,41 @@
-// const INPUT_1 = ["4", "-+0++++--+"];
+const INPUT_1 = ["4", "-+0++++--+"];
 
-// const input = INPUT_1;
+const input = INPUT_1;
 
-// // Solution
-// const solution = (input) => {
-//   // Variables
-//   const N = parseInt(input[0]);
-//   const sign = input[1].split("");
-//   const S = new Array(N);
-//   let start = 0;
-//   for (let i = 0; i < N; i++) {
-//     S[i] = new Array(N - i);
-//     for (let j = 0; j < N - i; j++) {
-//       S[i][j] = sign[start + j];
-//     }
-//     start += N - i;
-//   }
+// Solution
+const solution = (input) => {
+  // Variables
+  const N = parseInt(input[0]);
+  const sign = input[1].split("");
+  const S = new Array(N);
+  let start = 0;
+  for (let i = 0; i < N; i++) {
+    S[i] = new Array(N - i);
+    for (let j = 0; j < N - i; j++) {
+      S[i][j] = sign[start + j];
+    }
+    start += N - i;
+  }
 
-//   // Functions
-//   const fits = (index, cur) => {
-//     for(let i = 0; i <cur; i++) {
-//       if(S[i][index] === "-") {
+  // Functions
+  const fits = (index, cur) => {
+    for (let i = 0; i < cur; i++) {
+      if (S[i][index] === "-") {
+      }
+    }
+  };
 
-//       }
-//     }
-//   }
+  const go = (index) => {
+    if (index === N) {
+    }
+  };
 
-//   const go = (index) => {
-//     if(index === N) {
+  const solve = () => {
+    // console.log(S);
+  };
 
-//     }
-//   };
+  // Run
+  solve();
+};
 
-//   const solve = () => {
-//     // console.log(S);
-//   };
-
-//   // Run
-//   solve();
-// };
-
-// solution(input);
+solution(input);
