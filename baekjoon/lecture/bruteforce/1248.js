533b95c896549784805e6d3f832e0b35ea6a7c61
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-11-24 20:43:25 +0900

    Update 1248.js

--- baekjoon/lecture/bruteforce/1248.js
+++ baekjoon/lecture/bruteforce/1248.js
@@ -1,43 +1,64 @@
-const INPUT_1 = ["4", "-+0++++--+"];
+// const INPUT_1 = ["4", "-+0++++--+"];
 
-const input = INPUT_1;
+// const input = INPUT_1;
 
-// Solution
-const solution = (input) => {
-  // Variables
-  const N = parseInt(input[0]);
-  const sign = input[1].split("");
-  const S = new Array(N);
-  let start = 0;
-  for (let i = 0; i < N; i++) {
-    S[i] = new Array(N - i);
-    for (let j = 0; j < N - i; j++) {
-      S[i][j] = sign[start + j];
-    }
-    start += N - i;
-  }
+// // Solution
+// const solution = (input) => {
+//   // Variables
+//   const N = parseInt(input[0]);
+//   const sign = input[1].split("");
+//   const S = new Array(N);
+//   let start = 0;
+//   for (let i = 0; i < N; i++) {
+//     S[i] = new Array(N);
+//     for (let j = i; j < N; j++) {
+//       S[i][j] = sign[start];
+//       start++;
+//     }
+//   }
+//   let answer = new Array(N);
 
-  // Functions
-  const fits = (index, cur) => {
-    for (let i = 0; i < cur; i++) {
-      if (S[i][index] === "-") {
-      }
-    }
-  };
+//   // Functions
+//   const fits = (index) => {
+//     let sum = 0;
+//     for (let i = index; i >= 0; i--) {
+//       sum += answer[i];
+//       const flag = S[i][index];
+//       if (flag === "-") {
+//         if (sum >= 0) return false;
+//       } else if (flag === "0") {
+//         if (sum !== 0) return false;
+//       } else if (flag === "+") {
+//         if (sum <= 0) return false;
+//       }
+//     }
+//     return true;
+//   };
 
-  const go = (index) => {
-    // never fits
-    console.log(N);
-    console.log(sign);
-    console.log("I will be back!");
-  };
+//   const go = (index) => {
+//     if (index === N) {
+//       console.log(answer.join(" "));
+//       process.exit();
+//     }
+//     const curSign = S[index][index];
+//     if (curSign === "0") {
+//       answer[index] = 0;
+//       if (fits(index)) go(index + 1);
+//       return;
+//     }
+//     for (let i = 1; i <= 10; i++) {
+//       answer[index] = curSign === "+" ? i : -1 * i;
+//       if (fits(index)) go(index + 1);
+//     }
+//     return;
+//   };
 
-  const solve = () => {
-    // console.log(S);
-  };
+//   const solve = () => {
+//     go(0);
+//   };
 
-  // Run
-  solve();
-};
+//   // Run
+//   solve();
+// };
 
-solution(input);
+// solution(input);
