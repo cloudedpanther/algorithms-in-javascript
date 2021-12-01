50eca1cfd32c3e8ba3981868f1caab46fe10d6b5
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-12-01 22:01:07 +0900

    Update 13023.js

--- baekjoon/lecture/graph/13023.js
+++ baekjoon/lecture/graph/13023.js
@@ -1,40 +1,51 @@
-const INPUT_1 = ["5 4", "0 1", "1 2", "2 3", "3 4"];
+// const INPUT_1 = ["5 4", "0 1", "1 2", "2 3", "3 4"];
+// const INPUT_2 = ["6 5", "0 1", "0 2", "0 3", "0 4", "0 5"];
 
-const input = INPUT_1;
+// const input = INPUT_2;
 
-// Solution
-const solution = (input) => {
-  // Variables
-  const fl = input[0].split(" ");
-  const N = Number(fl[0]); // number of people
-  const M = Number(fl[1]); // number of relations
-  let list = new Array(N);
-  for (let i = 0; i < N; i++) {
-    list[i] = [];
-  }
+// // Solution
+// const solution = (input) => {
+//   // Variables
+//   const fl = input[0].split(" ");
+//   const N = Number(fl[0]); // number of people
+//   const M = Number(fl[1]); // number of relations
+//   let list = new Array(N);
+//   for (let i = 0; i < N; i++) {
+//     list[i] = [];
+//   }
 
-  // Functions
-  const connect = (index, past) => {
-    for (let i = 0; i < list[index].length; i++) {
-      const nextIndex = list[index][i];
-      if (!past.includes(nextIndex)) connect(nextIndex, past.push(nextIndex));
-    }
-  };
+//   // Functions
+//   const connect = (index, past) => {
+//     if (past.length === 5) {
+//       console.log(1);
+//       process.exit();
+//     }
+//     for (let i = 0; i < list[index].length; i++) {
+//       const nextIndex = list[index][i];
+//       if (past.indexOf(nextIndex) === -1) {
+//         connect(nextIndex, past.concat([nextIndex]));
+//       }
+//     }
+//   };
 
-  const solve = () => {
-    for (let i = 1; i <= M; i++) {
-      const cur = input[i].split(" ");
-      const n1 = Number(cur[0]);
-      const n2 = Number(cur[1]);
-      list[n1].push(n2);
-      list[n2].push(n1);
-    }
+//   const solve = () => {
+//     for (let i = 1; i <= M; i++) {
+//       const cur = input[i].split(" ");
+//       const n1 = Number(cur[0]);
+//       const n2 = Number(cur[1]);
+//       list[n1].push(n2);
+//       list[n2].push(n1);
+//     }
 
-    for (let i = 0; i < N; i++) {}
-  };
+//     for (let i = 0; i < N; i++) {
+//       connect(i, [i]);
+//     }
+//     console.log(0);
+//     return;
+//   };
 
-  // Run
-  solve();
-};
+//   // Run
+//   solve();
+// };
 
-solution(input);
+// solution(input);
