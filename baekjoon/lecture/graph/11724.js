1d0cf9239f5b0c4db8c43414d5eeaf0c3acf5652
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-12-03 23:02:51 +0900

    Create 11724.js

---
+++ baekjoon/lecture/graph/11724.js
@@ -0,0 +1,18 @@
+const INPUT_1 = [];
+
+const input = INPUT_1;
+
+// Solution
+const solution = (input) => {
+  // Variables
+
+  // Functions
+  const solve = () => {
+    //
+  };
+
+  // Run
+  solve();
+};
+
+solution(input);
