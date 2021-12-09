c76413fe0ba4ca00c42880cda76ac910e5e1ceae
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-12-09 22:44:33 +0900

    Create 2667.js

---
+++ baekjoon/lecture/graph/2667.js
@@ -0,0 +1,46 @@
+const INPUT_1 = [];
+
+const input = INPUT_1;
+
+// Solution
+const solution = (input) => {
+  // Variables
+
+  // Functions
+  const solve = () => {
+    //
+  };
+
+  // Run
+  solve();
+};
+
+solution(input);
+const INPUT_1 = [
+  "7",
+  "0110100",
+  "0110101",
+  "1110101",
+  "0000111",
+  "0100000",
+  "0111110",
+  "0111000",
+];
+
+const input = INPUT_1;
+
+// Solution
+const solution = (input) => {
+  // Variables
+  const N = Number(input[0]);
+
+  // Functions
+  const solve = () => {
+    //
+  };
+
+  // Run
+  solve();
+};
+
+solution(input);
