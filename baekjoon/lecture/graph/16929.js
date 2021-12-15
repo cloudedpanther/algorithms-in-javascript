72efc45a5fcdef024bd80a3823df6595d306b1cf
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-12-15 21:42:19 +0900

    Update 16929.js

--- baekjoon/lecture/graph/16929.js
+++ baekjoon/lecture/graph/16929.js
@@ -9,8 +9,11 @@ const INPUT_1 = [
   "AAAAAB",
 ];
 const INPUT_2 = ["3 4", "AAAA", "ABCA", "AADA"];
+const INPUT_3 = ["3 4", "AAAA", "ABCA", "AAAA"];
+const INPUT_4 = ["4 4", "YYYR", "BYBY", "BBBY", "BBBY"];
+const INPUT_5 = ["2 13", "ABCDEFGHIJKLM", "NOPQRSTUVWXYZ"];
 
-const input = INPUT_2;
+const input = INPUT_5;
 
 // Solution
 const solution = (input) => {
@@ -22,23 +25,21 @@ const solution = (input) => {
   const dy = [1, -1, 0, 0];
   let board = new Array(N);
   let check = new Array(N);
-  let answer = "NO";
 
   // Functions
-  const dfs = (x, y, px, py, cycle) => {
-    check[x][y] = cycle;
+  const dfs = (x, y, cnt) => {
+    if (check[x][y] > 0) return cnt - check[x][y] >= 4;
+    check[x][y] = cnt;
     for (let k = 0; k < 4; k++) {
       const nx = x + dx[k];
       const ny = y + dy[k];
       if (0 <= nx && nx < N && 0 <= ny && ny < M) {
         if (board[nx][ny] === board[x][y]) {
-          if ((px !== nx || py !== ny) && check[nx][ny] === check[x][y]) {
-            answer = "YES";
-          }
-          if (!check[nx][ny]) dfs(nx, ny, x, y, cycle);
+          if (dfs(nx, ny, cnt + 1)) return true;
         }
       }
     }
+    return false;
   };
 
   const solve = () => {
@@ -46,16 +47,17 @@ const solution = (input) => {
       board[i] = input[i + 1].split("");
       check[i] = new Array(M).fill(0);
     }
-    let cycle = 1;
     for (let i = 0; i < N; i++) {
       for (let j = 0; j < M; j++) {
-        if (!check[i][j]) {
-          dfs(i, j, i, j, cycle);
-          cycle++;
+        if (check[i][j] === 0) {
+          if (dfs(i, j, 1)) {
+            console.log("YES");
+            return;
+          }
         }
       }
     }
-    console.log(answer);
+    console.log("NO");
   };
 
   // Run
