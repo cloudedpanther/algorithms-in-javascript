8c0354ac884069ad3ab386a2748aa2cd2448de42
cloudedpanther <rickeykoh@ajou.ac.kr>
2021-12-16 22:46:55 +0900

    Create 16947.js

---
+++ baekjoon/lecture/graph/16947.js
@@ -0,0 +1,36 @@
+const INPUT_1 = ["6", "1 2", "3 4", "6 4", "2 3", "1 3", "3 5"];
+
+const input = INPUT_1;
+
+// Solution
+const solution = (input) => {
+  // Variables
+  const N = Number(input[0]);
+  let list = new Array(N + 1);
+  let dist = new Array(N + 1).fill(-1);
+  let check = new Array(N + 1).fill(0);
+
+  // Functions
+  const findCycle = (index) => {
+    check[index] = 1;
+  };
+
+  const solve = () => {
+    for (let i = 1; i < N + 1; i++) {
+      list[i] = [];
+    }
+    for (let i = 0; i < N; i++) {
+      const line = input[i + 1].split(" ");
+      const n1 = Number(line[0]);
+      const n2 = Number(line[1]);
+      list[n1].push(n2);
+      list[n2].push(n1);
+    }
+    console.log(list);
+  };
+
+  // Run
+  solve();
+};
+
+solution(input);
