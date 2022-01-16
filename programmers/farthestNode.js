598970f0a9f35e1083879f668514e6e8add7a535
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-16 11:34:05 +0900

    Update farthestNode.js

--- programmers/farthestNode.js
+++ programmers/farthestNode.js
@@ -4,11 +4,11 @@
 //     this.front = 0;
 //     this.rear = 0;
 //   }
-//   push(value) {
+//   enqueue(value) {
 //     this.queue.push(value);
 //     this.rear++;
 //   }
-//   pop() {
+//   dequeue() {
 //     const returnValue = this.queue[this.front];
 //     delete this.queue[this.front];
 //     this.front++;
@@ -21,37 +21,33 @@
 // }
 
 // function solution(n, edge) {
-//   let graph = new Array(n + 1);
-//   for (let i = 1; i < n + 1; i++) {
-//     graph[i] = [];
+//   let graph = Array.from(Array(n + 1), () => []);
+//   for (const [a, b] of edge) {
+//     graph[a].push(b);
+//     graph[b].push(a);
 //   }
-//   edge.forEach((e) => {
-//     graph[e[0]].push(e[1]);
-//     graph[e[1]].push(e[0]);
-//   });
+
+//   let distance = new Array(n + 1).fill(-1);
+//   distance[1] = 0;
 
 //   // BFS
-//   let visited = new Array(n + 1).fill(-1);
 //   let queue = new Queue();
-//   queue.push(1);
-//   visited[1] = 0;
+//   queue.enqueue(1);
 
 //   while (queue.size() > 0) {
-//     const cur = queue.pop();
-//     const currentNode = graph[cur];
-
-//     for (let i = 0; i < currentNode.length; i++) {
-//       const newNode = currentNode[i];
-//       if (visited[newNode] === -1) {
-//         queue.push(newNode);
-//         visited[newNode] = visited[cur] + 1;
+//     const cur = queue.dequeue();
+//     for (const node of graph[cur]) {
+//       if (distance[node] === -1) {
+//         queue.enqueue(node);
+//         distance[node] = distance[cur] + 1;
 //       }
 //     }
 //   }
 
-//   const maxLevel = visited.sort((a, b) => b - a)[0];
-//   const maxLevelNodes = visited.filter((e) => e === maxLevel).length;
-//   console.log(maxLevelNodes);
+//   const maxLevel = Math.max(...distance);
+//   //   return distance.filter((e) => e === maxLevel).length;
+//   const farthestNodes = distance.filter((e) => e === maxLevel).length;
+//   console.log(farthestNodes);
 // }
 
 // const n = 6;
