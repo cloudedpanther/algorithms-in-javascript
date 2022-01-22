6e0aadc2f15b59bfbbfec486b7757d21b7acbe66
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-22 23:06:50 +0900

    Create submitForm.js

---
+++ baekjoon/submitForm.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function solution(input) {
+  //
+}
+
+const input = [];
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution(input);
+  process.exit();
+});
