2404ee48761b70e03b99172dc9568c80c5b8351f
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-28 13:17:02 +0900

    Update secretMap.js

--- programmers/secretMap.js
+++ programmers/secretMap.js
@@ -1,25 +1,12 @@
-function toSharpBits(num, n) {
-  const stack = [];
-  while (num > 0) {
-    stack.push(num % 2);
-    num = Math.floor(num / 2);
-  }
-  while (stack.length < n) {
-    stack.push(0);
-  }
-  return stack
-    .reverse()
-    .map((cell) => (cell === 1 ? "#" : " "))
-    .join("");
-}
-
 function solution(n, arr1, arr2) {
   const answer = [];
-  const mergedMap = [];
   for (let i = 0; i < n; i++) {
-    mergedMap.push(arr1[i] | arr2[i]);
+    const merged = arr1[i] | arr2[i];
+    const bits = merged.toString(2);
+    const blankString = " ".repeat(n - bits.length);
+    const bitString = bits.replace(/1/gi, "#").replace(/0/gi, " ");
+    answer.push(blankString + bitString);
   }
-  mergedMap.forEach((row) => answer.push(toSharpBits(row, n)));
 
   return answer;
 }
