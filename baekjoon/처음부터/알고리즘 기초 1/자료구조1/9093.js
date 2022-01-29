0f5968572a4414230e4e3f85704795aabdca334b
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-29 16:54:56 +0900

    Create 9093.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/자료구조1/9093.js
@@ -0,0 +1,19 @@
+// const INPUT_1 = ["2", "I am happy today", "We want to win the first prize"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   let answer = [];
+//   for (const originalString of input.slice(1)) {
+//     // 공백으로 구분되는 각 단어마다 뒤집어서 출력.
+//     const stringArray = originalString.split(" ");
+//     const reversedArray = stringArray.map((e) =>
+//       e.split("").reverse().join("")
+//     );
+//     const reversedString = reversedArray.join(" ");
+//     answer.push(reversedString);
+//   }
+//   console.log(answer.join("\n"));
+// };
+
+// solution(input);
