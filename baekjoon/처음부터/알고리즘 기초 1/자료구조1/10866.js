2278a0d28dcdf69e78c53ca4909798d6184e9897
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-29 22:21:22 +0900

    Create 10866.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/자료구조1/10866.js
@@ -0,0 +1,47 @@
+const INPUT_1 = [
+  "15",
+  "push_back 1",
+  "push_front 2",
+  "front",
+  "back",
+  "size",
+  "empty",
+  "pop_front",
+  "pop_back",
+  "pop_front",
+  "size",
+  "empty",
+  "pop_back",
+  "push_front 3",
+  "empty",
+  "front",
+];
+
+const input = INPUT_1;
+
+// push_front X: 정수 X를 덱의 앞에 넣는다.
+// push_back X: 정수 X를 덱의 뒤에 넣는다.
+// pop_front: 덱의 가장 앞에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+// pop_back: 덱의 가장 뒤에 있는 수를 빼고, 그 수를 출력한다. 만약, 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+// size: 덱에 들어있는 정수의 개수를 출력한다.
+// empty: 덱이 비어있으면 1을, 아니면 0을 출력한다.
+// front: 덱의 가장 앞에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+// back: 덱의 가장 뒤에 있는 정수를 출력한다. 만약 덱에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+// class Deque {
+//   constructor() {
+//     this.deque = [];
+//     this.start = 0;
+//     this.end = 0;
+//   }
+//   push_front(value)  {
+
+//   }
+// }
+
+const solution = (input) => {
+  const d = new Deque();
+  const a = "hi";
+};
+
+solution(input);
