da0fccb6a4df0d8af18ca651ee8cd5bafe584b24
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-29 22:21:11 +0900

    Create 1406.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/자료구조1/1406.js
@@ -0,0 +1,28 @@
+// const INPUT_1 = ["abc", "9", "L", "L", "L", "L", "L", "P x", "L", "B", "P y"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const left = [];
+//   const right = [];
+//   input[0].split("").forEach((e) => left.push(e));
+//   for (const [command, value] of input.slice(2).map((e) => e.split(" "))) {
+//     switch (command) {
+//       case "L":
+//         if (left.length > 0) right.push(left.pop());
+//         break;
+//       case "D":
+//         if (right.length > 0) left.push(right.pop());
+//         break;
+//       case "B":
+//         if (left.length > 0) left.pop();
+//         break;
+//       case "P":
+//         left.push(value);
+//         break;
+//     }
+//   }
+//   console.log(left.join("") + right.reverse().join(""));
+// };
+
+// solution(input);
