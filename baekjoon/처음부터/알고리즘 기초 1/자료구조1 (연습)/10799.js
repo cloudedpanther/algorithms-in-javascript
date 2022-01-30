bc41fd0a46c00f228e67816113dc60ea85951ee5
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-30 15:36:01 +0900

    Create 10799.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/자료구조1 (연습)/10799.js
@@ -0,0 +1,23 @@
+// const INPUT_1 = ["()(((()())(())()))(())"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   let answer = 0;
+//   let laser = false;
+//   let bars = -1;
+//   for (const char of input[0]) {
+//     if (char === "(") {
+//       bars++;
+//       laser = true;
+//     } else {
+//       if (laser === true) answer += bars;
+//       else answer++;
+//       bars--;
+//       laser = false;
+//     }
+//   }
+//   console.log(answer);
+// };
+
+// solution(input);
