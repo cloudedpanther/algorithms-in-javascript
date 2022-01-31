76e81207ba1bffe3f45ac04db65dfd9a4d56d345
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-31 22:18:52 +0900

    Create 11656.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/자료구조1(참고)/11656.js
@@ -0,0 +1,13 @@
+// const INPUT_1 = ["baekjoon"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const suffix = [];
+//   for (let i = 0; i < input[0].length; i++) {
+//     suffix.push(input[0].slice(i));
+//   }
+//   console.log(suffix.sort().join("\n"));
+// };
+
+// solution(input);
