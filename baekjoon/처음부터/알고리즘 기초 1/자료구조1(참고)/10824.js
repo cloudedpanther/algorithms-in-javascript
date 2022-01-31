7ec88b40bd6bd4f5885f9d60542b2c3e376db1c7
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-31 22:18:45 +0900

    Create 10824.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/자료구조1(참고)/10824.js
@@ -0,0 +1,10 @@
+// const INPUT_1 = ["10 20 30 40"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const [a, b, c, d] = input[0].split(" ");
+//   console.log(parseInt(a + b) + parseInt(c + d));
+// };
+
+// solution(input);
