527856922641937ad7911a27781ada0444d9b491
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-31 22:18:48 +0900

    Create 11655.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/자료구조1(참고)/11655.js
@@ -0,0 +1,19 @@
+// const INPUT_1 = ["One is 1"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const answer = input[0].split("").map((e) => {
+//     const ascii = e.charCodeAt(0);
+//     if (65 <= ascii && ascii <= 90) {
+//       const decodedAscii = ascii - 13 < 65 ? ascii + 13 : ascii - 13;
+//       return String.fromCharCode(decodedAscii);
+//     } else if (97 <= ascii && ascii <= 122) {
+//       const decodedAscii = ascii - 13 < 97 ? ascii + 13 : ascii - 13;
+//       return String.fromCharCode(decodedAscii);
+//     } else return e;
+//   });
+//   console.log(answer.join(""));
+// };
+
+// solution(input);
