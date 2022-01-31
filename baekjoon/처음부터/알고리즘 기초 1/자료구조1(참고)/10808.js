d1738db32f2dae9dfb3b754adb8e5ce89fa0c2a9
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-01-31 22:18:32 +0900

    Create 10808.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/자료구조1(참고)/10808.js
@@ -0,0 +1,13 @@
+// const INPUT_1 = ["baekjoon"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   let alphabets = new Array(26).fill(0);
+//   for (let i = 0; i < input[0].length; i++) {
+//     alphabets[input[0].charCodeAt(i) - 97]++;
+//   }
+//   console.log(alphabets.join(" "));
+// };
+
+// solution(input);
