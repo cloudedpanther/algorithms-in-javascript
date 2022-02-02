b45c90e37c85c9263b4742e16e0d472e2d11e2b4
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-02 21:13:58 +0900

    Create 1676.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/수학1/1676.js
@@ -0,0 +1,25 @@
+// const INPUT_1 = ["500"];
+
+// const input = INPUT_1;
+
+// function factorial(N) {
+//   let factorial = 1n;
+//   for (let i = 2n; i <= N; i++) {
+//     factorial *= i;
+//   }
+//   return factorial;
+// }
+
+// const solution = (input) => {
+//   const N = BigInt(input[0]);
+//   let num = factorial(N);
+//   let answer = 0;
+
+//   while (num % 10n === 0n) {
+//     num /= 10n;
+//     answer++;
+//   }
+//   console.log(answer);
+// };
+
+// solution(input);
