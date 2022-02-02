f89d9259d3a6eb82cdc3dca5c9b2380330ebdcde
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-02 21:13:56 +0900

    Create 1676(다른풀이).js

---
+++ baekjoon/처음부터/알고리즘 기초 1/수학1/1676(다른풀이).js
@@ -0,0 +1,14 @@
+// const INPUT_1 = ["500"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const N = parseInt(input[0]);
+//   let answer = 0;
+//   for (let i = 5; i <= 125; i *= 5) {
+//     answer += Math.floor(N / i);
+//   }
+//   console.log(answer);
+// };
+
+// solution(input);
