74e5894c3d6804da54b1edbcc486af0228a24d59
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-02 17:11:39 +0900

    Create 10430.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/수학1/10430.js
@@ -0,0 +1,18 @@
+// const INPUT_1 = ["5 8 4"];
+
+// const input = INPUT_1;
+
+// // 첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+
+// const solution = (input) => {
+//   const [a, b, c] = input[0].split(" ").map(Number);
+//   const result = [
+//     (a + b) % c,
+//     ((a % c) + (b % c)) % c,
+//     (a * b) % c,
+//     ((a % c) * (b % c)) % c,
+//   ];
+//   result.forEach((r) => console.log(r));
+// };
+
+// solution(input);
