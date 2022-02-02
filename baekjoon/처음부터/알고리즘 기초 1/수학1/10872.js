0c0b29a3d2c29acc2f411ef975639f5c0fc9f4df
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-02 21:13:51 +0900

    Create 10872.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/수학1/10872.js
@@ -0,0 +1,14 @@
+// const INPUT_1 = ["10"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const N = parseInt(input[0]);
+//   let factorial = 1;
+//   for (let i = 2; i <= N; i++) {
+//     factorial *= i;
+//   }
+//   console.log(factorial);
+// };
+
+// solution(input);
