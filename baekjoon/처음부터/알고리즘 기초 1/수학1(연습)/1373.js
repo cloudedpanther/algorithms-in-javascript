ba2e1d56a766214becaaac9f38720e8e88119f10
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-03 21:37:14 +0900

    Create 1373.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/수학1(연습)/1373.js
@@ -0,0 +1,17 @@
+// const INPUT_1 = ["11001100"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   let answer = "";
+//   const binary = input[0].split("").map(Number);
+//   while (binary.length % 3 !== 0) {
+//     binary.unshift(0);
+//   }
+//   for (let i = 0; i < binary.length; i += 3) {
+//     answer += binary[i] * 4 + binary[i + 1] * 2 + binary[i + 2];
+//   }
+//   console.log(answer);
+// };
+
+// solution(input);
