ae109ebd057b4dac9269ff307adb738a3f2d2e56
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-03 21:37:08 +0900

    Create 2089.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/수학1(연습)/2089.js
@@ -0,0 +1,20 @@
+// const INPUT_1 = ["-13"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   let num = parseInt(input[0]);
+//   const stack = [];
+
+//   while (Math.abs(num) > 0) {
+//     const rem = num % -2;
+//     if (rem === -1) stack.push(1);
+//     else stack.push(rem);
+
+//     num = Math.ceil(num / -2);
+//   }
+//   if (stack.length === 0) stack.push(0);
+//   console.log(stack.reverse().join(""));
+// };
+
+// solution(input);
