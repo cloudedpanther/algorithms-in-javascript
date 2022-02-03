d6af1355772eee1db52ca0546244dc2cc54b03d5
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-03 13:38:15 +0900

    Create 2609(다른풀이).js

---
+++ baekjoon/처음부터/알고리즘 기초 1/수학1/2609(다른풀이).js
@@ -0,0 +1,25 @@
+// const INPUT_1 = ["24 18"];
+
+// const input = INPUT_1;
+
+// function getGcd(a, b) {
+//   while (a % b !== 0) {
+//     const tmp = a % b;
+//     a = b;
+//     b = tmp;
+//   }
+//   return b;
+// }
+
+// const solution = (input) => {
+//   const [n1, n2] = input[0]
+//     .split(" ")
+//     .map(Number)
+//     .sort((a, b) => a - b);
+//   const gcd = getGcd(n1, n2);
+//   const lcm = (n1 * n2) / gcd;
+//   console.log(gcd);
+//   console.log(lcm);
+// };
+
+// solution(input);
