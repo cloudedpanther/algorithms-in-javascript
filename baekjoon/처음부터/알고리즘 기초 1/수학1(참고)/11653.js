8053903c00519d537cbe4edb058fa77e39889d8c
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-05 21:48:06 +0900

    Create 11653.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/수학1(참고)/11653.js
@@ -0,0 +1,18 @@
+// const INPUT_1 = ["72"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const answer = [];
+//   let N = parseInt(input[0]);
+//   for (let i = 2; i <= N; i++) {
+//     while (N % i === 0) {
+//       answer.push(i);
+//       N /= i;
+//     }
+//     if (N === 1) break;
+//   }
+//   console.log(answer.join("\n"));
+// };
+
+// solution(input);
