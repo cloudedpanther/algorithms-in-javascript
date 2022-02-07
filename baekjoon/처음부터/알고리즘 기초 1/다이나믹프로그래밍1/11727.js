9b3550e841dbd3ca16bef1a42596f7e83166b6ab
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-07 21:55:10 +0900

    Create 11727.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/다이나믹프로그래밍1/11727.js
@@ -0,0 +1,15 @@
+// const INPUT_1 = ["12"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const d = 10007;
+//   const n = parseInt(input[0]);
+//   let dp = new Array(n + 1).fill(1);
+//   for (let i = 2; i < n + 1; i++) {
+//     dp[i] = ((dp[i - 1] % d) + (((dp[i - 2] % d) * 2) % d)) % d;
+//   }
+//   console.log(dp[n]);
+// };
+
+// solution(input);
