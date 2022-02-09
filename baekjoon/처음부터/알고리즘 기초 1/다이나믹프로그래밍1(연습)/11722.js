6a8155717134502db4f057b872eb9a8add728872
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-09 21:05:16 +0900

    Create 11722.js

---
+++ baekjoon/처음부터/알고리즘 기초 1/다이나믹프로그래밍1(연습)/11722.js
@@ -0,0 +1,17 @@
+// const INPUT_1 = ["6", "10 30 10 20 20 10"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const N = parseInt(input[0]);
+//   const seq = input[1].split(" ").map(Number);
+//   let dp = new Array(N).fill(1);
+//   for (let i = 1; i < N; i++) {
+//     for (let j = 0; j < i; j++) {
+//       if (seq[j] > seq[i]) dp[i] = Math.max(dp[i], dp[j] + 1);
+//     }
+//   }
+//   console.log(Math.max(...dp));
+// };
+
+// solution(input);
