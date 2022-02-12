66bd02a45d78959d0e033f73b8543709fd48f02a
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-13 00:49:50 +0900

    Create 1476.js

---
+++ baekjoon/처음부터/알고리즘 기초 2/브루트포스/1476.js
@@ -0,0 +1,15 @@
+// const INPUT_1 = ["1 2 3"];
+
+// const input = INPUT_1;
+
+// const solution = (input) => {
+//   const [E, S, M] = input[0].split(" ").map(Number);
+//   for (let i = 0; i <= 532; i++) {
+//     if ((15 * i + E - S) % 28 === 0 && (15 * i + E - M) % 19 === 0) {
+//       console.log(15 * i + E);
+//       return;
+//     }
+//   }
+// };
+
+// solution(input);
