7356fbde7ec21e27d41adea144f6781256dbcee8
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-18 17:17:57 +0900

    Create 15654.js

---
+++ baekjoon/처음부터/알고리즘 기초 2/브루트포스(N과M)/15654.js
@@ -0,0 +1,33 @@
+// const INPUT_1 = ["4 2", "9 8 7 1"];
+
+// const input = INPUT_1;
+
+// function go(N, M, index, numbers, chosen, answer) {
+//   if (index === M) {
+//     answer.push([...chosen].join(" "));
+//     return;
+//   }
+
+//   for (let i = 0; i < N; i++) {
+//     const cur = numbers[i];
+//     if (!chosen.has(cur)) {
+//       chosen.add(cur);
+//       go(N, M, index + 1, numbers, chosen, answer);
+//       chosen.delete(cur);
+//     }
+//   }
+// }
+
+// const solution = (input) => {
+//   const [N, M] = input[0].split(" ").map(Number);
+//   const numbers = input[1]
+//     .split(" ")
+//     .map(Number)
+//     .sort((a, b) => a - b);
+//   const answer = [];
+//   const chosen = new Set();
+//   go(N, M, 0, numbers, chosen, answer);
+//   console.log(answer.join("\n"));
+// };
+
+// solution(input);
