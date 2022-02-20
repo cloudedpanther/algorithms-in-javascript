21741202fd3f9d3c2c9ff08e8ce03f2921f48951
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-02-20 22:26:39 +0900

    Update 10972.js

--- baekjoon/처음부터/알고리즘 기초 2/브루트포스(순열)/10972.js
+++ baekjoon/처음부터/알고리즘 기초 2/브루트포스(순열)/10972.js
@@ -1,41 +1,36 @@
-const INPUT_1 = ["4", "1 2 3 4"];
-const INPUT_2 = ["4", "4 3 2 1"];
+// const INPUT_1 = ["4", "1 2 3 4"];
+// const INPUT_2 = ["4", "4 3 2 1"];
+// const INPUT_3 = ["5", "3 5 4 2 1"];
 
-const input = INPUT_1;
+// const input = INPUT_3;
 
-const solution = (input) => {
-  const N = parseInt(input[0]);
-  const seq = input[1].split(" ").map(Number);
-  let stillPart = [];
-  let newPart = false;
-  for (let i = N - 1; i > 0; i--) {
-    if (seq[i - 1] < seq[i]) {
-      stillPart = seq.slice(0, i - 1);
-      newPart = seq.slice(i - 1, N).reverse();
-      break;
-    }
-  }
-  if (!newPart) {
-    console.log(-1);
-    return;
-  }
+// const solution = (input) => {
+//   const N = parseInt(input[0]);
+//   const seq = input[1].split(" ").map(Number);
+//   const tail = [];
+//   let headEnd = -1;
+//   for (let i = N - 1; i > 0; i--) {
+//     tail.push(seq[i]);
+//     if (seq[i - 1] < seq[i]) {
+//       headEnd = i - 1;
+//       break;
+//     }
+//   }
+//   if (headEnd === -1) {
+//     console.log(headEnd);
+//     return;
+//   }
 
-  const oldHead = newPart.pop();
-  let stack = [];
-  while (newPart.length > 0) {
-    const cur = newPart.pop();
-    if (cur > oldHead) {
-      stack.push(cur);
-      if (newPart.length > 0) continue;
-    }
-    const newHead = stack.pop();
-    stack.push(oldHead);
-    while (newPart.length > 0) {
-      stack.push(newPart.pop());
-    }
-    stack = [newHead, ...stack.reverse()];
-  }
-  console.log([...stillPart, ...stack].join(" "));
-};
+//   let answer = seq.slice(0, headEnd);
+//   for (let i = 0; i < tail.length; i++) {
+//     if (tail[i] > seq[headEnd]) {
+//       answer.push(tail[i]);
+//       tail[i] = seq[headEnd];
+//       break;
+//     }
+//   }
+//   answer = [...answer, ...tail];
+//   console.log(answer.join(" "));
+// };
 
-solution(input);
+// solution(input);
