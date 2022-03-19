5e450283ce616f145829836a977dbe409f47087c
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-03-19 23:24:02 +0900

    Update 1991.js

--- baekjoon/처음부터/알고리즘 기초 2/트리1/1991.js
+++ baekjoon/처음부터/알고리즘 기초 2/트리1/1991.js
@@ -11,8 +11,36 @@ const INPUT_1 = [
 
 const input = INPUT_1;
 
+function dfs(tree, visited, order, currentIndex) {
+  visited[currentIndex] = true;
+  order.push(tree[currentIndex]);
+  const leftIndex = currentIndex * 2;
+  const rightIndex = currentIndex * 2 + 1;
+  if (!visited[leftIndex] && !!tree[leftIndex]) {
+    dfs(tree, visited, order, leftIndex);
+  }
+  if (!visited[rightIndex] && !!tree[rightIndex]) {
+    dfs(tree, visited, order, rightIndex);
+  }
+}
+
+function preorderTraversal(tree) {
+  const visited = new Array(tree.length).fill(false);
+  const order = [];
+  dfs(tree, visited, order, 1);
+  return order.join(" ");
+}
+
 const solution = (input) => {
-  //
+  const tree = [null, "A"];
+  input.slice(1).forEach((e) => {
+    const [parent, left, right] = e.split(" ");
+    const parentIndex = tree.findIndex((v, i) => v === parent);
+    if (left !== ".") tree[parentIndex * 2] = left;
+    if (right !== ".") tree[parentIndex * 2 + 1] = right;
+  });
+  const preorder = preorderTraversal(tree);
+  console.log(preorder);
 };
 
 solution(input);
